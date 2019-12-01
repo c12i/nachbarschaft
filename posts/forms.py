ddd74984a9f3b7775504ca8d69a4382939a9e79c
Collins Muriuki <murerwacollins@gmail.com>
2019-12-01 20:48:16 +0300

    Fix posts app forms module typos

--- posts/forms.py
+++ posts/forms.py
@@ -5,14 +5,14 @@ from posts import models
 
 class PostForm(forms.ModelForm):
     class Meta:
-        fields = ("message", "neighborhood")
+        fields = ("message", "group")
         model = models.Post
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user", None)
         super().__init__(*args, **kwargs)
         if user is not None:
-            self.fields["neighborhood"].queryset = (
+            self.fields["group"].queryset = (
                 models.Group.objects.filter(
                     pk__in=user.groups.values_list("group__pk")
                 )
