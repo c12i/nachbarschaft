29fc02976b9cf3ad89f80c58b9f9c4f597594079
Collins Muriuki <murerwacollins@gmail.com>
2019-12-02 12:03:55 +0300

    Configure search results url for neighborhood app

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 from . import views
 
@@ -10,8 +10,9 @@ urlpatterns = [
     url(r"^$", views.ListGroups.as_view(), name="all"),
     url(r"^new/$", views.CreateGroup.as_view(), name="create"),
     url(r"^posts/in/(?P<slug>[-\w]+)/$",views.SingleGroup.as_view(),name="single"),
-    url(r"join/(?P<slug>[-\w]+)/$",views.JoinGroup.as_view(),name="join"),
-    url(r"leave/(?P<slug>[-\w]+)/$",views.LeaveGroup.as_view(),name="leave"),
+    url(r"^join/(?P<slug>[-\w]+)/$",views.JoinGroup.as_view(),name="join"),
+    url(r"^leave/(?P<slug>[-\w]+)/$",views.LeaveGroup.as_view(),name="leave"),
+    url(r"^search-results/$", views.search_results, name="search")
 ]
 
 # if settings.DEBUG:
--- neighborhood/views.py
+++ neighborhood/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
-from django.contrib.auth.mixins import(
-    LoginRequiredMixin,
-    PermissionRequiredMixin
-)
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
 from django.shortcuts import get_object_or_404
